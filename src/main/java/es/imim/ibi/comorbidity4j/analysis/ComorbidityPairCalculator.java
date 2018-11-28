7534feb928b650fc591fc3f3d50942cdb5d307c9
fra82 <fr.ronzano@gmail.com>
2018-11-28 19:45:20 +0100

    Minor updates - enable sex ratio analysis

--- src/main/java/es/imim/ibi/comorbidity4j/analysis/ComorbidityPairCalculator.java
+++ src/main/java/es/imim/ibi/comorbidity4j/analysis/ComorbidityPairCalculator.java
@@ -332,7 +332,7 @@ public class ComorbidityPairCalculator {
 			}
 			
 			// 7) Comorbidity Sex Ratio
-			/** MP **/
+			/** MP *
 			comPairResult.setFemaleWithDisA(0);
 			comPairResult.setFemaleWithDisB(0);
 			comPairResult.setFemaleWithDisAandB(0);
@@ -341,9 +341,9 @@ public class ComorbidityPairCalculator {
 			comPairResult.setMaleWithDisAandB(0);
 			comPairResult.setSexRatioAB(0d);
 			comPairResult.setSexRatioBA(0d);
-			/*
-			computeSexRatio(diseaseA_ID, diseaseB_ID, comMiner, computationCache, comPairResult, femalePatFilter, malePatFilter);
 			*/
+			computeSexRatio(diseaseA_ID, diseaseB_ID, comMiner, computationCache, comPairResult, femalePatFilter, malePatFilter);
+			
 			
 			if(comMiner.getScoreFilter() != null && !comMiner.getScoreFilter().checkScoreFilters(comPairResult, false)) {
 				// The comorbidity pair did not pass the filter
